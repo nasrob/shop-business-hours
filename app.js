19d18ca11ec45744a929a6576bf2a833afde6076
Nasser BOUKEHIL <boukehil.nasreddine@gmail.com>
2021-06-28 20:52:47 +0200

    added express and template

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 require('dotenv').config()
+const express = require('express')
+const path = require('path')
 const base = require('airtable').base('appCkqr3mz2NqlZ1B')
 
-;(async () => {
-    const records = await base('Business Hours')
-        .select({
-        view: 'Grid view',
-        }).firstPage()
+const app = express()
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, '.'))
+
+app.get('/', (req, res) => {
+    (async () => {
+        const records = await base('Business Hours')
+            .select({
+            view: 'Grid view',
+            }).firstPage()
+        
+        // for (const record of records) {
+        //     console.log(record.get('Day'), record.get('Hours'))
+        // }
+        res.render('page', { records })
+    })()
     
-    for (const record of records) {
-        console.log(record.get('Day'), record.get('Hours'))
-    }
-})()
+})
+
+app.listen(3000, () => console.log('Server Ready'))
