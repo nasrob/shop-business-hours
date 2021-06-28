3db2deae9dda1daa50b32d9f420f39bb455fc014
Nasser BOUKEHIL <boukehil.nasreddine@gmail.com>
2021-06-28 21:22:39 +0200

    added a simple cache mechanism

--- app.js
+++ app.js
@@ -7,19 +7,28 @@ const app = express()
 app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, '.'))
 
+let records
+
 app.get('/', (req, res) => {
-    (async () => {
-        const records = await base('Business Hours')
-            .select({
-            view: 'Grid view',
-            }).firstPage()
-        
-        // for (const record of records) {
-        //     console.log(record.get('Day'), record.get('Hours'))
-        // }
+    if (records) {
+        console.log('cached')
         res.render('page', { records })
-    })()
-    
+    } else {
+        (async () => {
+            records = await base('Business Hours')
+                .select({
+                view: 'Grid view',
+                }).firstPage()
+
+            res.render('page', { records })
+
+            // to refrech fetching records from airtable
+            setTimeout(() => {
+                records = null
+            }, 10 * 1000)
+
+        })()
+    }
 })
 
 app.listen(3000, () => console.log('Server Ready'))
